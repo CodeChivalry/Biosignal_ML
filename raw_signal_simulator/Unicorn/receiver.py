c74755268070342ce6c2db8670028ff7e10537cb
Mehak Sharma <122295036+CodeChivalry@users.noreply.github.com>
2024-06-03 16:14:20 +0530

    Update receiver.py

--- raw_signal_simulator/Unicorn/receiver.py
+++ raw_signal_simulator/Unicorn/receiver.py
@@ -1,9 +1,7 @@
 from pylsl import StreamInlet, resolve_stream
-print("Looking for an unicorn stream...")
+
+print("Looking for a unicorn stream...")
 streams = resolve_stream('type', 'unicorn')
-
-
-
 
 inlet = StreamInlet(streams[0])
 
@@ -13,6 +11,7 @@ try:
     while True:
         # Get a new sample (sample is a list of float values)
         sample, timestamp = inlet.pull_sample()
+        # Print the timestamp and sample
         print(f"Timestamp: {timestamp}, Sample: {sample}")
 except KeyboardInterrupt:
     print("Data reception stopped by user.")
